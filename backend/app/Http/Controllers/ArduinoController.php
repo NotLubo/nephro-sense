21736905af5fc7cce5859b56329846ccc407cbfc
Ľuboš Repka <40338867+NotLubo@users.noreply.github.com>
2021-11-21 15:46:55 +0100

    intersystem API added

--- backend/app/Http/Controllers/ArduinoController.php
+++ backend/app/Http/Controllers/ArduinoController.php
@@ -55,27 +55,96 @@ class ArduinoController extends Controller
             if ($oldItem) {
                 continue;
             }
-            PatientData::query()->create([
+            $item = PatientData::query()->create([
                 'last_value' => $item['last_value'],
                 'value_updated_at' => $item['value_updated_at'],
                 'variable_name' => $item['variable_name'],
             ]);
+            if ($item) {
+                $this->sendToIsCloud(['id' => $item->id, 'text' => $item->variable_name, 'value' => $item->last_value]);
+            }
         }
     }
 
     public function getAllData(Request $request)
     {
         $centrTemps = PatientData::query()->where('variable_name', 'centrTemp')->where('created_at', '>=', Carbon::now()->subHour())->get();
-        $perifTemps = PatientData::query()->where('variable_name', 'perifTemp ')->where('created_at', '>=', Carbon::now()->subHour())->get();
-        $weights = PatientData::query()->where('variable_name', 'vaha')->where('created_at', '>=', Carbon::now()->subHour())->get();
+        $perifTemps = PatientData::query()->where('variable_name', 'perifTemp')->where('created_at', '>=', Carbon::now()->subHour())->get();
+        $weights = PatientData::query()->where('variable_name', 'vaha')->where('created_at', '>=', Carbon::now()->subDay())->get();
+        $lastHourWeights = PatientData::query()->where('variable_name', 'vaha')->where('created_at', '>=', Carbon::now()->subHour())->get();
 
         return response()->json(['status' => 'success', 'data' => [
             'actualCentrTemp' => $centrTemps->isNotEmpty() ? $centrTemps->last() : null,
             'actualPerifTemp' => $perifTemps->isNotEmpty() ? $perifTemps->last() : null,
             'actualWeight' => $weights->isNotEmpty() ? $weights->last() : null,
+            'weightOneHourBefore' => $lastHourWeights->isNotEmpty() ? $lastHourWeights->last() : null,
             'centrTemps' => $centrTemps,
             'perifTemps' => $perifTemps,
             'weights' => $weights,
         ]]);
+    }
+
+    public function sendToIsCloud($entry)
+    {
+        $mapText = [
+          'vaha' => 'urine',
+          'centrTemp' => 'central temp',
+          'perifTemp' => 'periferal temp',
+        ];
+
+        $id = 'ID-' . $entry['id'];
+        $text = $mapText[$entry['text']];
+        $value = $entry['value'];
+        $unit = $entry['text'] == 'vaha' ? 'ml' : 'celsius';
+
+        $data = [
+            'resourceType' => 'Observation',
+            'id' => $id,
+            'meta' => [
+                'profile' => [
+                    'http://hl7.org/fhir/StructureDefinition/vitalsigns'
+                ]
+            ],
+            'status' => 'final',
+            'category' => [
+                [
+                    'coding' => [
+                        [
+                            'system' => 'http://terminology.hl7.org/CodeSystem/observation-category',
+                            'code' => 'vital-signs',
+                            'display' => 'Vital Signs'
+                        ]
+                    ],
+                    'text' => 'Vital Signs'
+                ]
+            ],
+            'code' => [
+                'coding' => [
+                    [
+                        'system' => 'http://loinc.org',
+                        'code' => '8302-2',
+                        'display' => 'Urine'
+                    ]
+                ],
+                'text' => $text
+            ],
+            'subject' => [
+                'reference' => 'Patient/example'
+            ],
+            'effectiveDateTime' => Carbon::now()->format('Y-m-d'),
+            'valueQuantity' => [
+                'value' => $value,
+                'unit' => $unit,
+                'system' => 'http://unitsofmeasure.org',
+                'code' => '[in_i]'
+            ]
+        ];
+
+        $response = Http::withHeaders(['x-api-key' => 'bNFrmoQjpN7B9UzFxfWldh81WdG4qPl6l6DYuFnd'])
+            ->post('https://fhir.n3o2k67smq89.static-test-account.isccloud.io/Observation', $data);
+
+        if (!$response->successful()) {
+            Log::alert('Cannot send data to cloud. Code: ' . $response->status());
+        }
     }
 }
